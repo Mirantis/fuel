cf282191e9c64bb85cfeec300e3b3cee9c7f76ae
Bogdan Dobrelya <bogdando@mail.ru>
2013-05-13 15:37:10 +0300

    Update mysql module v0.6.1
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- deployment/puppet/mysql/manifests/config.pp
+++ deployment/puppet/mysql/manifests/config.pp
@@ -6,7 +6,7 @@
 #   [*old_root_password*] - previous root user password,
 #   [*bind_address*]      - address to bind service.
 #   [*port*]              - port to bind service.
-#   [*etc_root_password*] - whether to save /etc/.my.cnf.
+#   [*etc_root_password*] - whether to save /etc/my.cnf.
 #   [*service_name*]      - mysql service name.
 #   [*config_file*]       - my.cnf configuration file path.
 #   [*socket*]            - mysql socket.
@@ -15,6 +15,10 @@
 #   [*ssl_ca]             - path to ssl-ca
 #   [*ssl_cert]           - path to ssl-cert
 #   [*ssl_key]            - path to ssl-key
+#   [*log_error]          - path to mysql error log
+#   [*default_engine]     - configure a default table engine
+#   [*root_group]         - use specified group for root-owned files
+#   [*restart]            - whether to restart mysqld (true/false)
 #
 # Actions:
 #
@@ -46,14 +50,19 @@ class mysql::config(
   $ssl_key           = $mysql::params::ssl_key,
   $log_error         = $mysql::params::log_error,
   $default_engine    = 'UNSET',
-  $root_group        = $mysql::params::root_group
+  $root_group        = $mysql::params::root_group,
+  $restart           = $mysql::params::restart,
+  $purge_conf_dir    = false
 ) inherits mysql::params {
 
   File {
     owner  => 'root',
     group  => $root_group,
     mode   => '0400',
-    notify => Exec['mysqld-restart'],
+    notify    => $restart ? {
+      true => Exec['mysqld-restart'],
+      false => undef,
+    },
   }
 
   if $ssl and $ssl_ca == undef {
@@ -82,15 +91,18 @@ class mysql::config(
   if $root_password != 'UNSET' {
     case $old_root_password {
       '':      { $old_pw='' }
-      default: { $old_pw="-p${old_root_password}" }
+      default: { $old_pw="-p'${old_root_password}'" }
     }
 
     exec { 'set_mysql_rootpw':
-      command   => "mysqladmin -u root ${old_pw} password ${root_password}",
+      command   => "mysqladmin -u root ${old_pw} password '${root_password}'",
       logoutput => true,
-      unless    => "mysqladmin -u root -p${root_password} status > /dev/null",
+      unless    => "mysqladmin -u root -p'${root_password}' status > /dev/null",
       path      => '/usr/local/sbin:/usr/bin:/usr/local/bin',
-      notify    => Exec['mysqld-restart'],
+      notify    => $restart ? {
+        true => Exec['mysqld-restart'],
+        false => undef,
+      },
       require   => File['/etc/mysql/conf.d'],
     }
 
@@ -105,6 +117,10 @@ class mysql::config(
         require => Exec['set_mysql_rootpw'],
       }
     }
+  } else {
+    file { '/root/.my.cnf':
+      ensure  => present,
+    }
   }
 
   file { '/etc/mysql':
@@ -112,10 +128,11 @@ class mysql::config(
     mode   => '0755',
   }
   file { '/etc/mysql/conf.d':
-    ensure => directory,
-    mode   => '0755',
+    ensure  => directory,
+    mode    => '0755',
+    recurse => $purge_conf_dir,
+    purge   => $purge_conf_dir,
   }
-
   file { $config_file:
     content => template('mysql/my.cnf.erb'),
     mode    => '0644',
--- deployment/puppet/mysql/manifests/init.pp
+++ deployment/puppet/mysql/manifests/init.pp
@@ -13,12 +13,12 @@
 #
 class mysql (
   $package_name   = $mysql::params::client_package_name,
-  $package_ensure = $mysql::params::client_version
+  $package_ensure = 'present'
 ) inherits mysql::params {
 
-  package { 'mysql-client':
-    name    => $package_name,
-   #ensure  => $package_ensure,
+  package { 'mysql_client':
+    ensure => $package_ensure,
+    name   => $package_name,
   }
 
 }
