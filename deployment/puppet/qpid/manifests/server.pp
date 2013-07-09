4dadfcf228b191ae33885263f320e49ba632872e
ilyinon <ilyinon@mail.ru>
2013-07-09 04:46:42 -0700

    Update server.pp

--- deployment/puppet/qpid/manifests/server.pp
+++ deployment/puppet/qpid/manifests/server.pp
@@ -14,24 +14,66 @@ class qpid::server(
   $qpid_cluster = false,
   $qpid_cluster_name = 'qpid_cluster',
   $qpid_username = 'nova',
-  $qpid_password = 'nova'
+  $qpid_password = 'nova',
+  $qpid_nodes = [$::ipaddress],
 ) {
 
   validate_re($auth, '^(yes$|no$)')
 
   include qpid::params
-
+  include stdlib
   package { $::qpid::params::package_name:
     ensure => $package_ensure,
   }
 
-  if $qpid_cluster {
-    package { $::qpid::params::cluster_package_name:
-      ensure => $package_ensure,
-      require => Package[$::qpid::params::package_name],
+  define qpid_safe_package(){
+    if ! defined(Package[$name]){
+      @package { $name : }
     }
   }
 
+
+  if size($qpid_nodes) > 1 {
+    package { [$qpid::params::additional_packages]:
+      ensure => $package_ensure,
+    }
+    qpid_safe_package { $qpid::params::additional_packages : }
+    
+    file { '/usr/local/bin/qpid-setup-routes.sh':
+      ensure => present,
+      owner => 'root',
+      group => 'root',
+      mode => 755,
+      content => template('qpid/qpid-setup-routes.sh.erb'),
+    }
+
+    exec { "propagate_qpid_routes":
+      path    => "/usr/bin/:/bin:/usr/sbin",
+      command => "bash /usr/local/bin/qpid-setup-routes.sh",
+      subscribe => File['/usr/local/bin/qpid-setup-routes.sh'],
+      logoutput => true,
+    }
+  }
+#
+#  $qpid_nodes_arr = inline_template("<%= (qpid_nodes).join('\n') + \"\n\" %>")
+#
+##
+#  file { '/tmp/qpid-exec.sh':
+#    source => 'puppet:///modules/qpid/qpid-exec.sh',
+#  }
+#
+#  file { "/tmp/qpid-endpoints.txt":
+#     content => $qpid_nodes_arr,
+#  }
+#
+#  exec { "propagate_qpid_routes":
+#    path    => "/usr/bin/:/bin:/usr/sbin",
+#    command => "bash /tmp/qpid-exec.sh",
+#    require => File[ '/tmp/qpid-exec.sh',
+#             '/tmp/qpid-endpoints.txt' ],
+#    logoutput => "on_failure",
+#  }
+#
   if $auth == 'yes' {
     qpid_user { 'qpid_user':
       password => $qpid_password,
@@ -50,7 +92,6 @@ class qpid::server(
       before => File[$::qpid::params::config_file]
     }
   }
-
   file { $::qpid::params::config_file:
     ensure => present,
     owner => 'root',
@@ -58,7 +99,6 @@ class qpid::server(
     mode => 644,
     content => template('qpid/qpidd.conf.erb'),
     require => Package[$::qpid::params::package_name],
-    notify => Service[$::qpid::params::service_name],
   }
 
   if $log_to_file != 'UNSET' {
@@ -68,20 +108,26 @@ class qpid::server(
       group => 'qpidd',
       mode => 644,
       require => Package[$::qpid::params::package_name],
+      before => File[$::qpid::params::config_file],
     }
   }
-
   if $qpid_cluster {
+    exec { 'qpid-corosync-restart':
+      path => '/usr/bin/:/usr/sbin:/bin:/sbin',
+      command => "/sbin/service corosync restart",
+      before => Service[$::qpid::params::service_name],
+      require => [File[$::qpid::params::config_file],
+  	  Package[$::qpid::params::cluster_package_name]]
+    }
     service { $::qpid::params::service_name:
       enable => true,
       ensure => $service_ensure,
       hasstatus  => true,
       hasrestart => true,
-      subscribe => [Exec['corosync-restart'],
-                    File[$::qpid::params::config_file]],
+      subscribe => File[$::qpid::params::config_file],
       require => [Package[$::qpid::params::package_name],
                   File[$::qpid::params::config_file],
-                  Exec['corosync-restart']],
+                  Exec['qpid-corosync-restart']],
     }
   }
   else {
@@ -92,5 +138,6 @@ class qpid::server(
       hasrestart => true,
       require => [Package[$::qpid::params::package_name], File[$::qpid::params::config_file]],
     }
+
   }
 }
