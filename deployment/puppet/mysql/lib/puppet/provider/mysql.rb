openstack-archive/fuel-library

mysql remote host support

deployment/puppet/mysql/lib/puppet/provider/mysql.rb
@@ -1,0 +1,14 @@
+class Puppet::Provider::Mysql < Puppet::Provider
+  def connection_options
+    cmd_array = []
+    if @resource[:host]
+      cmd_array = [
+        "--host=#{@resource[:host]}",
+        "--port=#{@resource[:port]}",
+        "--user=#{@resource[:authorized_user]}",
+        "--password=#{@resource[:authorized_pass]}",
+      ]
+    end
+    cmd_array
+  end
+end

Mirantis/fuel

deployment/puppet/mysql/lib/puppet/provider/mysql.rb
@@ -1,0 +1,14 @@
+class Puppet::Provider::Mysql < Puppet::Provider
+  def connection_options
+    cmd_array = []
+    if @resource[:connection_host]
+      cmd_array = [
+        "--host=#{@resource[:connection_host]}",
+        "--port=#{@resource[:connection_port]}",
+        "--user=#{@resource[:connection_user]}",
+        "--password=#{@resource[:connection_pass]}",
+      ]
+    end
+    cmd_array
+  end
+end