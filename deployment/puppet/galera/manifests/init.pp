d752d416a6e6ee2a04af20f30e2949c7cc8d16fb
Andrey Korolyov <andrey@xdel.ru>
2013-07-16 13:03:58 +0400

    add first completion flag

--- deployment/puppet/galera/manifests/init.pp
+++ deployment/puppet/galera/manifests/init.pp
@@ -236,7 +236,12 @@ class galera (
     try_sleep => 5,
     tries     => 60,
   }
-  
+
+  exec { "raise-first-setup-flag" :
+   path    => "/usr/bin:/usr/sbin:/bin:/sbin",
+   command => "crm_attribute -t crm_config --name mysqlprimaryinit --update done",
+  }
+
   Package["MySQL-server"] -> Exec["set-mysql-password"] 
   File['/tmp/wsrep-init-file'] -> Exec["set-mysql-password"] -> Exec["wait-initial-sync"] 
   -> Exec["kill-initial-mysql"] -> Service["mysql-galera"] -> Exec ["wait-for-synced-state"]
@@ -260,6 +265,7 @@ class galera (
       onlyif    => "[ -f /var/lib/mysql/grastate.dat ] && (cat /var/lib/mysql/grastate.dat | awk '\$1 == \"uuid:\" {print \$2}' | awk '{if (\$0 == \"00000000-0000-0000-0000-000000000000\") exit 0; else exit 1}')",
       require    => Service["mysql-galera"],
       before     => Exec ["wait-for-synced-state"],
+      notify     => Exec ["raise-first-setup-flag"],
     }
   }
 }
