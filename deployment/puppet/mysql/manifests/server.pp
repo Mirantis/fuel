61e62848841a0c863de81a73f8e19dc3bcd8723c
Andrey Korolyov <andrey@xdel.ru>
2013-07-18 16:58:14 +0400

    Revert "initial corosync"
    This reverts commit 4c2670406058e5c679d304e9667306bba8484e1c.

--- deployment/puppet/mysql/manifests/server.pp
+++ deployment/puppet/mysql/manifests/server.pp
@@ -29,46 +29,7 @@ class mysql::server (
   $mysql_skip_name_resolve = false,
   $use_syslog              = false,
 ) inherits mysql::params {
-
- if $service_provider == 'pacemaker' {
-
-    cs_resource { "mysql":
-      ensure          => present,
-      cib             => 'mysql',
-      primitive_class => 'ocf',
-      provided_by     => 'pacemaker',
-      primitive_type  => 'mysql',
-      require => File['mysql-wss'],
-      operations      => {
-        'monitor'  => {
-          'interval' => '20',
-          'timeout'  => '30'
-        }
-        ,
-        'start'    => {
-          'timeout' => '420'
-        }
-        ,
-        'stop'     => {
-          'timeout' => '420'
-       }
-     }
-   }
-   Cs_commit['mysql'] -> Service['mysql']
-   cs_shadow { 'mysql': cib => 'mysql' }
-   cs_commit { 'mysql': cib => 'mysql' }
-   service { 'mysql':
-     name       => "mysql",
-     enable     => $enabled,
-     ensure     => $ensure,
-     hasstatus  => true,
-     hasrestart => false,
-     provider   => $service_provider,
-     require    => [Package[mysql-server], Class['mysql'], Service['mysql']],
-    }
-
-
- } else {
+    
   if ($custom_setup_class == undef) {
     include mysql
     Class['mysql::server'] -> Class['mysql::config']
@@ -117,5 +78,4 @@ class mysql::server (
    else {
     require($custom_setup_class)
   }
- }
 }
