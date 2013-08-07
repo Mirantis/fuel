fe22223fc0f6f9486794abc277dd42221de645c1
Vladimir Kuklin <vkuklin@mirantis.com>
2013-08-07 16:06:13 +0400

    add licenses

--- deployment/puppet/osnailyfacter/manifests/rpmcache.pp
+++ deployment/puppet/osnailyfacter/manifests/rpmcache.pp
@@ -1,3 +1,18 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
 class osnailyfacter::rpmcache {
 
   $rh_base_channels = "rhel-6-server-rpms rhel-6-server-optional-rpms rhel-lb-for-rhel-6-server-rpms rhel-rs-for-rhel-6-server-rpms rhel-ha-for-rhel-6-server-rpms rhel-server-ost-6-folsom-rpms"
