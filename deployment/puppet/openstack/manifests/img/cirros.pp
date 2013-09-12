8be6d62e1101ae0cd500b9a1c8974c1c6ac90925
Nikita Gubenko <nikita.gubenko@gmail.com>
2013-09-12 19:13:20 +0400

    cirros image version update

--- deployment/puppet/openstack/manifests/img/cirros.pp
+++ deployment/puppet/openstack/manifests/img/cirros.pp
@@ -24,7 +24,7 @@ case $::osfamily {
    }
   'Debian': {
   exec { 'upload-img':
-    command => "/usr/bin/glance -N ${os_auth_url} -T ${os_tenant_name} -I ${os_username} -K ${os_password} add name=${img_name} is_public=${public} container_format=${container_format} disk_format=${disk_format} distro=${os_name} < /usr/share/cirros-testvm/cirros-0.3.0-x86_64-disk.img",
+    command => "/usr/bin/glance -N ${os_auth_url} -T ${os_tenant_name} -I ${os_username} -K ${os_password} add name=${img_name} is_public=${public} container_format=${container_format} disk_format=${disk_format} distro=${os_name} < /usr/share/cirros-testvm/cirros-0.3.1-x86_64-disk.img",
     unless => "/usr/bin/glance -N ${os_auth_url} -T ${os_tenant_name} -I ${os_username} -K ${os_password} index && (/usr/bin/glance -N ${os_auth_url} -T ${os_tenant_name} -I ${os_username} -K ${os_password} index | grep ${img_name})",
 
     }
