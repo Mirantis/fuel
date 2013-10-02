b160efc03e23b6080ae3eb77f725a98aab9e1fe3
Max Mazur <mmaxur@mirantis.com>
2013-10-02 20:16:16 +0300

    savanna 0.2.2

--- deployment/puppet/savanna/manifests/dashboard.pp
+++ deployment/puppet/savanna/manifests/dashboard.pp
@@ -1,51 +1,128 @@
 # Installs & configure the savanna API service
 
 class savanna::dashboard (
-  $enabled            = true,
-  $settings_py        = '/usr/share/openstack-dashboard/openstack_dashboard/settings.py',
-  $local_settings     = '/etc/openstack-dashboard/local_settings',
-  $savanna_url_string = "SAVANNA_URL = 'http://localhost:8386/v1.0'"
+  $enabled                      = true,
+  $settings_py                  = '/usr/share/openstack-dashboard/openstack_dashboard/settings.py',
+  $local_settings               = '/etc/openstack-dashboard/local_settings',
+  $savanna_url_string           = "SAVANNA_URL = 'http://localhost:8386/v1.0'",
+  $use_neutron                  = 'True',
+  $floating_ips_auto_assigment  = 'False',
 ) {
 
 
+
   include savanna::params
+  include stdlib
+
+  if $use_neutron == 'True' {
+    $floating_ips_auto_assigment_status = 'False'
+  }
+  else {
+    if $floating_ips_auto_assigment  == 'False' {
+      $floating_ips_auto_assigment_status = 'False'
+    }
+    else {
+      $floating_ips_auto_assigment_status = 'True'
+    }
+  }
 
   if $enabled {
-    $service_ensure = 'running'
-    $line_in_settings_py__ensure = 'present'
+    $line_ensure = 'present'
     $package_ensure = 'installed'
 
   } else {
-    $service_ensure = 'stopped'
-    $line_in_settings_py_ensure = 'absent'
+    $line_ensure = 'absent'
     $package_ensure = 'absent'
   }
 
-
-
-  file { "$settings_py":
-     alias => "horizon_settings",
-     ensure => present,
-     notify => Service['httpd'],
-
+  File_line {
+    ensure => $line_ensure,
   }
 
-  file_line{'savanna'          : path => $settings_py,    line => "HORIZON_CONFIG['dashboards'].append('savanna')", ensure => $line_in_settings_py_ensure, }
-  file_line{'savannadashboard' : path => $settings_py,    line => "INSTALLED_APPS.append('savannadashboard')",      ensure => $line_in_settings_py_ensure, }
-  file_line{'savannaurl'       : path => $local_settings, line => "${savanna_url_string}",                          ensure => $line_in_settings_py_ensure, notify => Service['httpd'], }
+  if !defined(File[$settings_py]) {
+    file { $settings_py :
+      ensure  => present,
+    }
+  }
+
+  if !defined(File[$local_settings]) {
+    file { $local_settings :
+      ensure  => present,
+    }
+  }
+
+  file_line{'savanna' :
+    path    => $settings_py,
+    line    => "HORIZON_CONFIG['dashboards'].append('savanna')",
+    require => File[$settings_py],
+  }
+
+  file_line{'savanna_dashboard' :
+    path    => $settings_py,
+    line    => "INSTALLED_APPS.append('savannadashboard')",
+    require => File[$settings_py],
+  }
+
+  file_line{'savanna_url' :
+    path    => $local_settings,
+    line    => $savanna_url_string,
+    require => File[$local_settings],
+  }
+
+
+
+  if  $use_neutron == 'False' {
+    file_line{'savanna_use_neutron_false' :
+      path    => $local_settings,
+      line    => "SAVANNA_USE_NEUTRON=False",
+    }
+  }
+  else {
+    file_line{'savanna_use_neutron_true' :
+      path    => $local_settings,
+      line    => "SAVANNA_USE_NEUTRON=True",
+    }
+  }
+
+
+  if $floating_ips_auto_assigment_status  == 'False' {
+    file_line{'savanna_ips_auto_assigment_false' :
+      path    => $local_settings,
+      line    => "AUTO_ASSIGNMENT_ENABLED=False",
+    }
+  }
+  else {
+    file_line{'savanna_ips_auto_assigment_true' :
+      path    => $local_settings,
+      line    => "AUTO_ASSIGNMENT_ENABLED=True",
+    }
+  }
+
+
+
+
+
+
 
   package { 'savanna_dashboard':
     ensure => $package_ensure,
     name   => $::savanna::params::savanna_dashboard_package_name,
-    notify => Service['httpd'],
   }
 
-
-  service { 'httpd':
-    ensure     => running,
-    name       => httpd,
-    enable     => $enabled,
-  }
-
+  File_line <| title == 'savanna' or title == 'savanna_dashboard' or title == 'savanna_url' |> ~> Service <| title == 'httpd' |>
+  File <| title == $settings_py or title == $local_settings |> ~> Service <| title == 'httpd' |>
+  Package['savanna_dashboard'] -> File <| title == $settings_py or title == $local_settings |>
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- deployment/puppet/savanna/manifests/params.pp
+++ deployment/puppet/savanna/manifests/params.pp
@@ -3,9 +3,17 @@ class savanna::params {
   case $::osfamily {
     'RedHat': {
       # package names
-      $savanna_package_name = 'openstack-savanna-virtualenv-savanna'
+      $savanna_package_name           = 'openstack-savanna-virtualenv-savanna'
+      $savanna_dashboard_package_name = 'savanna-dashboard'
       # service names
-      $savanna_service_name = 'openstack-savanna-api'
+      $savanna_service_name           = 'openstack-savanna-api'
+    }
+    'Debian': {
+      # package names
+      $savanna_package_name           = 'openstack-savanna-virtualenv-savanna'
+      $savanna_dashboard_package_name = 'savanna-dashboard'
+      # service names
+      $savanna_service_name           = 'openstack-savanna-api'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem: \
