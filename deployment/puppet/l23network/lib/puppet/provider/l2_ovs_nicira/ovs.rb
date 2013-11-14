fe5547269cc2d4add24b217a79fe7c7e7143ddc1
Dmitry Nikishov <dnikishov@mirantis.com>
2013-11-14 13:02:30 +0400

    Refactored l2_ovs_nicira.rb:   Added get_uuid method   Added destroy method   Added certificate generation
    Change-Id: I06cd031bb8d53d83f1616732c5096f1686d37b64

--- deployment/puppet/l23network/lib/puppet/provider/l2_ovs_nicira/ovs.rb
+++ deployment/puppet/l23network/lib/puppet/provider/l2_ovs_nicira/ovs.rb
@@ -4,6 +4,36 @@ require 'json'
 
 Puppet::Type.type(:l2_ovs_nicira).provide(:ovs) do
 
+  commands :vsctl => '/usr/bin/ovs-vsctl'
+  commands :vspki => '/usr/bin/ovs-pki'
+
+  @cert_dir = "/etc/openvswitch"
+  @cert_path = "#{@cert_dir}/ovsclient-cert.pem"
+  @privkey_path = "#{@cert_dir}/ovsclient-privkey.pem"
+  @cacert_path = "#{@cert_dir}/vswitchd.cacert"
+
+  def generate_cert
+    if not File.exists? @cert_path
+      old_dir = Dir.pwd
+      Dir.chdir "/etc/openvswitch"
+      vspki("init --force")
+      vspki("req+sign ovsclient controller")
+      vsctl("-- --bootstrap set-ssl #{@cert_path} #{@privkey_path} #{@cacert_path}")
+      vsctl("set-manager ssl:#{@resource[:nsx_endpoint]}")
+      Dir.chdir old_dir
+    end
+  end
+
+  def get_cert
+    if File.exists? @cert_path
+      cert_file = File.open(@cert_path, "r")
+      cert_contents = cert_file.read
+      cert = cert_contents.gsub(/.*?(?=-*BEGIN CERTIFICATE-*)/m, "")
+      return cert
+    end
+    return nil
+  end
+
   def login
     conn = Net::HTTP.new(@resource[:nsx_endpoint],443)
     conn.use_ssl = true
@@ -12,26 +42,35 @@ Puppet::Type.type(:l2_ovs_nicira).provide(:ovs) do
     return conn, cookie
   end
 
-  def exists?
-    conn, cookie = login
-    resp, data = conn.get('/ws.v1/transport-node', { 'Cookie' => cookie })
+  def get_uuid(display_name)
+    resp, data = @conn.get('/ws.v1/transport-node', { 'Cookie' => @cookie })
     nodes = JSON.load(data)['results']
     nodes.each { |node|
-      resp, data = conn.get(node['_href'], { 'Cookie' => cookie })
+      resp, data = @conn.get(node['_href'], { 'Cookie' => @cookie })
       if JSON.load(data)['display_name'] == @resource[:name]
-        return true
+        return JSON.load(data)['uuid']
       end
     } 
-    false
+    return nil
+  end
+
+  def exists?
+    @conn, @cookie = login
+    query_result = get_uuid(@resource[:name])
+    if query_result != nil
+      return true
+    end
+    return false
   end
 
   def create
-    conn, cookie = login
+    generate_cert
+    cert = get_cert
     query = {
 	  'display_name' => @resource[:name],
 	  'credential' => {
 	    'client_certificate' => {
-	      'pem_encoded' => @resource[:client_certificate] 
+	      'pem_encoded' => cert 
 	    },
 	    'type' => 'SecurityCertificateCredential'
 	  },
@@ -44,9 +83,13 @@ Puppet::Type.type(:l2_ovs_nicira).provide(:ovs) do
 	  ],
 	  'integration_bridge_id' => @resource[:integration_bridge]
     }
-    conn.post('/ws.v1/transport-node', query.to_json, { 'Cookie' => cookie , 'Content-Type' => 'application/json'})
+    @conn.post('/ws.v1/transport-node', query.to_json, { 'Cookie' => @cookie , 'Content-Type' => 'application/json'})
   end
 
   def destroy
+    uuid = get_uuid(@resource[:name])
+    if uuid != nil
+      @conn.delete("/ws.v1/transport-node/#{uuid}", { 'Cookie' => @cookie})
+    end
   end
 end
